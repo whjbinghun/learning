68ea0e2e885f3a9f91a7080bb52658e700d90bf9
wanghengjin <wanghengjin@comteck.com>
2016-06-21 10:14:49 +0800

    修改SuSqlite类，并增加 增、删、改、查功能

--- TestSQLite/TestSQLite/SuSqlite.cpp
+++ TestSQLite/TestSQLite/SuSqlite.cpp
@@ -3,74 +3,99 @@
 #include <QtSql/QSqlQuery>
 #include <QtSql/QSqlError>
 #include <QtSql/QSqlDriver>
+#include <QDebug>
 
 SuSqlite::SuSqlite(QString str_data_base_name)
     : mb_sql_open( false )
 {
-    QSqlDatabase sql_db = QSqlDatabase::addDatabase( "QSQLITE" );
-    sql_db.setDatabaseName( str_data_base_name );
-    mb_sql_open = sql_db.open(); // 连接数据库, 然后就可以使用了.
+    m_sql_db = QSqlDatabase::addDatabase( "QSQLITE" );
+    m_sql_db.setDatabaseName( str_data_base_name );
+    mb_sql_open = m_sql_db.open(); // 连接数据库, 然后就可以使用了.
 
 }
 
 SuSqlite::~SuSqlite()
 {
-
+    if( mb_sql_open ) {
+        m_sql_db.close();
+        mb_sql_open = false;
+    }
 }
 
 bool SuSqlite::create_table( QString str_table_name )
 {
-//    QString str_create_table = QString( "create table %1 (id int primary key, name vchar" ).arg( str_table_name );
-//    if( m_sql_query.exec( QObject::tr( str_create_table ) ) ) {
-//        qDebug()<<"创建数据库表成功";
-//        ms_sql_table_name = str_table_name;
+    QString str_create_table = QString( "create table %1 (id int primary key, name vchar" ).arg( str_table_name );
+    if( m_sql_query.exec( QObject::tr( str_create_table.toStdString().c_str() ) ) ) {
+        qDebug()<<"创建数据库表成功";
+        ms_sql_table_name = str_table_name;
 
-//        return true;
-//    }
+        return true;
+    }
 
-//    return false;
+    return false;
 }
 
 bool SuSqlite::insert_data( int n_id, QString str_name )
 {
-//    QString str_insert = QString( "insert into %1 values( %2, %3 )" ).arg( ms_sql_table_name ).arg( n_id ).arg( str_name );
-//    if( m_sql_query.exec( QObject::tr( str_insert ) ) ) {
-//        qDebug()<<"插入数据成功";
-//        return true;
-//    }
+    QString str_insert = QString( "insert into %1 values( %2, %3 )" ).arg( ms_sql_table_name ).arg( n_id ).arg( str_name );
+    if( m_sql_query.exec( QObject::tr( str_insert.toStdString().c_str() ) ) ) {
+        qDebug()<<"插入数据成功";
+        return true;
+    }
 
-//    return false;
+    return false;
 }
 
 bool SuSqlite::update_data( int n_id, QString str_name )
 {
-//    QString str_update = QString( "update into %1 values( %2, %3 )" ).arg( ms_sql_table_name ).arg( n_id ).arg( str_name );
-//    if( m_sql_query.exec( QObject::tr( str_insert ) ) ) {
-//        qDebug()<<"插入数据成功";
-//        return true;
-//    }
+    QString str_update = QString( "update into %1 values( %2, %3 )" ).arg( ms_sql_table_name ).arg( n_id ).arg( str_name );
+    if( m_sql_query.exec( QObject::tr( str_update.toStdString().c_str() ) ) ) {
+        qDebug()<<"更新数据成功";
+        return true;
+    }
 
-//    return false;
+    return false;
 }
 
-bool SuSqlite::delete_data()
+bool SuSqlite::delete_data( int n_id )
 {
+    QString str_delete = QString( "delete into %1 where id = %2 " ).arg( ms_sql_table_name ).arg( n_id );
+    if( m_sql_query.exec( QObject::tr( str_delete.toStdString().c_str() ) ) ) {
+        qDebug()<<"删除数据成功";
+        return true;
+    }
 
+    return false;
 }
 
 bool SuSqlite::query_all_data()
 {
-//    QString str_query_all_data = QString( "select * from %1 " ).arg( ms_sql_table_name );
-//    if( m_sql_query.exec( QObject::tr( str_query_all_data ) ) )   //尝试列出  表的所有记录
-//    {
-//        return true;
-//    }
+    QString str_query_all_data = QString( "select * from %1 " ).arg( ms_sql_table_name );
+    if( m_sql_query.exec( QObject::tr( str_query_all_data.toStdString().c_str() ) ) )   //尝试列出  表的所有记录
+    {
+        while( m_sql_query.next() ) {//开始就先执行一次next()函数，那么query指向结果集的第一条记录
+            PersonInfo person_info;
+            person_info.n_id = m_sql_query.value( 0 ).toInt();
+            person_info.str_name = m_sql_query.value( 1 ).toString();
+            person_info.str_month = m_sql_query.value( 2 ).toString();
+            m_map_person_info.insert( person_info.n_id, person_info );
+        }
 
-//    return false;
+        return true;
+    }
+
+    return false;
 }
 
-bool SuSqlite::query_data()
+bool SuSqlite::query_data( int n_id )
 {
+    QString str_query_data = QString( "select * from %1 where id = %2 " ).arg( ms_sql_table_name ).arg( n_id );
+    if( m_sql_query.exec( QObject::tr( str_query_data.toStdString().c_str() ) ) )   //尝试列出  表的所有记录
+    {
+        qDebug()<<"查询数据成功";
+        return true;
+    }
 
+    return false;
 }
 
--- TestSQLite/TestSQLite/SuSqlite.h
+++ TestSQLite/TestSQLite/SuSqlite.h
@@ -2,6 +2,17 @@
 #define SUSQLITE_H
 #include <QObject>
 #include <QString>
+#include <QtSql/QSqlDatabase>
+#include <QtSql/QSqlQuery>
+#include <QtSql/QSqlError>
+#include <QtSql/QSqlDriver>
+#include <QMap>
+
+typedef struct _tagPersonInfo {
+    int n_id;
+    QString str_name;
+    QString str_month;
+}PersonInfo;
 
 class SuSqlite
 {
@@ -13,14 +24,19 @@ public:
     bool create_table( QString str_table_name );
     bool insert_data( int n_id, QString str_name );
     bool update_data( int n_id, QString str_name );
-    bool delete_data();
+    bool delete_data( int n_id );
     bool query_all_data();
-    bool query_data();
+    bool query_data( int n_id );
 
 private:
+    QSqlDatabase m_sql_db;
+    QSqlQuery m_sql_query;
+
     bool mb_sql_open;
     QString ms_data_base_name;
     QString ms_sql_table_name;
+
+    QMap<int, PersonInfo> m_map_person_info;
 };
 
 #endif // SUSQLITE_H
